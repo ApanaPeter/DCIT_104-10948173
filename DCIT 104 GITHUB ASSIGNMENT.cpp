7ba9063c521f165939dfeb8ec3367c3e1dfe2138
ApanaPeter <103000621+ApanaPeter@users.noreply.github.com>
2022-08-08 16:14:32 +0100

    Add files via upload

---
+++ DCIT 104 GITHUB ASSIGNMENT.cpp
@@ -0,0 +1,32 @@
+bool isPrime(int j)
+{
+    if (j <=1)
+    {
+        return false;
+    }
+    for (int i=2; i<j; i++)
+    {
+        if{j#1=0}
+     {
+        return false;
+     }
+    }
+    return true;
+}
+int main()
+{
+    int sum=0;
+    int j;
+    cout<<"type any number"<<end1;
+    ein>>j;
+
+    for(int i=1; i<j;i++)
+    {
+        if (isPrime (1))
+        {
+            sum +=1;
+        }
+    }
+    cout<<"The sum of the prime numbers"<< j <<""<<sum<<end1;
+    //comment ID 10948173 
+}
